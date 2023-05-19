66d138f35038b7d8aaf1050be64b489ea5eb1d27
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-05-19 15:57:47 +0300

    Add project files.

---
+++ PaperCastle/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PaperCastle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

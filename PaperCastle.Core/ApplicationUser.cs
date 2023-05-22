c42fe5695c12fd77cd1264c1afa9be90b30431fe
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-05-22 22:44:50 +0300

    added user entity

---
+++ PaperCastle.Core/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PaperCastle.Core
+{
+    public class ApplicationUser
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public ICollection<ApplicationUser> Friends { get; set; }
+        public ICollection<Book> BooksRead { get; set; }
+        public ICollection<Book> BooksCurrentlyReading { get; set; }
+        public ICollection<Book> BooksWantsToRead { get; set; } 
+
+    }
+}

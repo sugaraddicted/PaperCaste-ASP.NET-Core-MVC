5d9632b4ecc524d5cf231d769937bfd1fb3a5335
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-05-22 22:22:25 +0300

    added author entity

---
+++ PaperCastle.Core/Author.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PaperCastle.Core
+{
+    public class Author
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }  
+        public string Bio { get; set; }
+        public Country Country { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
--- PaperCastle.Core/Book.cs
+++ PaperCastle.Core/Book.cs
@@ -11,6 +11,7 @@ namespace PaperCastle.Core
         public string Description { get; set; }
         public string CoverImageURL { get; set; }
         public int YearOfWriting { get; set; }
+        public Author Author { get; set; }  
         public Country Country { get; set; }    
         public ICollection<Genre> Genres { get; set; }
 

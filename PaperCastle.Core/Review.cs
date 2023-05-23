6280206ccbf7e4661359d448588563320eac5f7e
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-05-23 12:46:13 +0300

    implemented review entity

--- PaperCastle.Core/Review.cs
+++ PaperCastle.Core/Review.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace PaperCastle.Core
 {
-    internal class Review
+    public class Review
     {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public Book Book { get; set; }
+        public decimal Rating { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
     }
 }

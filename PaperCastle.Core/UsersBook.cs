05992318eae7856cd7bbefe5f38dc2865b6f71df
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-05-24 15:48:35 +0300

    added users book entity

---
+++ PaperCastle.Core/UsersBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperCastle.Core
+{
+    public class UsersBook
+    {
+        public int ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+
+        BookStatus Status { get; set; } 
+    }
+}

34a7a7beb39291897a9db1d96d624dc546dcd7d2
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-05-30 10:23:04 +0300

    added genre repository

---
+++ PaperCastle.Infrastructure/Data/Intefaces/IGenreRepository.cs
@@ -0,0 +1,28 @@
+﻿using PaperCastle.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PaperCastle.Infrastructure.Data.Intefaces
+{
+    public interface IGenreRepository
+    {
+        ICollection<Genre> GetGenres();
+        Genre GetGenreById(int id);
+
+        ICollection<Book> GetBooksByGenre(int genreId);
+
+        bool GenreExists(int id);
+
+        bool CreateGenre(Genre genre);
+
+        bool Save();
+
+        bool UpdateGenre(Genre genre);
+
+        bool DeleteGenre(Genre genre);
+    }
+}

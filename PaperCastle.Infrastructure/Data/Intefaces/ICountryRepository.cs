4e6eec2343ae018c4f215c3019b87c718a07bca6
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-05-30 09:41:21 +0300

    added country repository

---
+++ PaperCastle.Infrastructure/Data/Intefaces/ICountryRepository.cs
@@ -0,0 +1,28 @@
+﻿using PaperCastle.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PaperCastle.Infrastructure.Data.Intefaces
+{
+    public interface ICountryRepository
+    {
+        ICollection<Country> GetCountries();
+        Country GetCountryById(int id);
+
+        ICollection<Book> GetBooksByCountry(int countryId);
+
+        bool CountryExists(int id);
+
+        bool CreateCountry(Country country);
+
+        bool UpdateCountry(Country country);
+
+        bool DeleteCountry(Country country);
+
+        bool Save();
+    }
+}
---
+++ PaperCastle.Infrastructure/Data/Repository/CountryRepository.cs
@@ -0,0 +1,58 @@
+﻿using PaperCastle.Core.Entity;
+using PaperCastle.Infrastructure.Data.Intefaces;
+
+namespace PaperCastle.Infrastructure.Data.Repository
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private readonly DataContext _context;
+
+        public CountryRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool CountryExists(int id)
+        {
+            return _context.Countries.Any(c => c.Id == id);
+        }
+
+        public ICollection<Book> GetBooksByCountry(int countryId)
+        {
+            return _context.Books.Where(a => a.Country.Id == countryId).ToList();
+        }
+
+        public ICollection<Country> GetCountries()
+        {
+            return _context.Countries.ToList();
+        }
+
+        public Country GetCountryById(int id)
+        {
+            return _context.Countries.Where(c => c.Id == id).FirstOrDefault();
+        }
+        public bool CreateCountry(Country country)
+        {
+            _context.Add(country);
+            return Save();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool UpdateCountry(Country country)
+        {
+            _context.Update(country);
+            return Save();
+        }
+
+        public bool DeleteCountry(Country country)
+        {
+            _context.Remove(country);
+            return Save();
+        }
+    }
+}

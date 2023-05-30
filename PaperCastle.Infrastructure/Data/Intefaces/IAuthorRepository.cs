b0472c2586ca8f86e7e7e1a815f607deeabb30b8
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-05-30 09:37:01 +0300

    added author repository

---
+++ PaperCastle.Infrastructure/Data/Intefaces/IAuthorRepository.cs
@@ -0,0 +1,28 @@
+﻿using PaperCastle.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PaperCastle.Infrastructure.Data.Intefaces
+{
+    public interface IAuthorRepository
+    {
+        ICollection<Author> GetAuthors();
+        Author GetAuthorById(int id);
+
+        ICollection<Book> GetAuthorsBooks(int authorsId);
+
+        bool AuthorExists(int id);
+
+        bool CreateAuthor(Author author);
+
+        bool Save();
+
+        bool UpdateAuthor(Author author);
+
+        bool DeleteAuthor(Author author);
+    }
+}
---
+++ PaperCastle.Infrastructure/Data/Repository/AuthorRepository.cs
@@ -0,0 +1,61 @@
+﻿using PaperCastle.Core.Entity;
+using PaperCastle.Infrastructure.Data.Intefaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace PaperCastle.Infrastructure.Data.Repository
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+        private readonly DataContext _context;
+
+        public AuthorRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<Book> GetAuthorsBooks(int authorId)
+        {
+            var author = _context.Authors.Include(a => a.Books).FirstOrDefault(a => a.Id == authorId);
+            return author?.Books.ToList();
+        }
+
+        public Author GetAuthorById(int id)
+        {
+            return _context.Authors.Where(a => a.Id == id).FirstOrDefault();
+        }
+
+        public ICollection<Author> GetAuthors()
+        {
+            return _context.Authors.ToList();
+        }
+
+        public bool AuthorExists(int id)
+        {
+            return _context.Authors.Any(a => a.Id == id);
+        }
+
+        public bool CreateAuthor(Author author)
+        {
+            _context.Add(author);
+            return Save();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool UpdateAuthor(Author author)
+        {
+            _context.Update(author);
+            return Save();
+        }
+
+        public bool DeleteAuthor(Author author)
+        {
+            _context.Remove(author);
+            return Save();
+        }
+    }
+}

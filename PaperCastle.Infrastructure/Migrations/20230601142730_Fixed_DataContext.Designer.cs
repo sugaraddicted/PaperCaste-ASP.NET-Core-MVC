0115d15322ca0d9032867d53ef627b865aef178b
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-06-01 17:28:27 +0300

    added migration Fixed_DataContext

--- PaperCastle.Infrastructure/Migrations/20230601142730_Fixed_DataContext.Designer.cs
+++ PaperCastle.Infrastructure/Migrations/20230601142730_Fixed_DataContext.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PaperCastle.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using PaperCastle.Infrastructure.Data;
 namespace PaperCastle.Infrastructure.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230601142730_Fixed_DataContext")]
+    partial class Fixed_DataContext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

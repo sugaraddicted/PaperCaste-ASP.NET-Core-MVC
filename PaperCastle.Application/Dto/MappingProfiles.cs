7f13c48bed8fcf595de5899e1d148a4356bb7720
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2023-06-19 16:26:51 +0300

    feature/dto implementation (#2)
    * added book dto
    
    * added author dto
    
    * added bookshelf dto
    
    * added country dto
    
    * added genre dto
    
    * added review dto
    
    * added automapper mapping profiles

---
+++ PaperCastle.Application/Dto/MappingProfiles.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using PaperCastle.Core.Entity;
+
+namespace PaperCastle.Application.Dto
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Book, BookDto>();
+            CreateMap<BookDto, Book>();
+
+            CreateMap<Genre, GenreDto>();
+            CreateMap<GenreDto, Genre>();
+
+            CreateMap<Country, CountryDto>();
+            CreateMap<CountryDto, Country>();
+
+            CreateMap<Author, AuthorDto>();
+            CreateMap<AuthorDto, Author>();
+
+            CreateMap<Review, ReviewDto>();
+            CreateMap<ReviewDto, Review>();
+
+            CreateMap<Bookshelf, BookshelfDto>();
+            CreateMap<BookshelfDto, Bookshelf>();
+        }
+    }
+}

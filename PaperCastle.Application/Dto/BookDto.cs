d03967799e33f8adf10d9a7124d456af19ebf82a
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-06-19 16:18:06 +0300

    added book dto

---
+++ PaperCastle.Application/Dto/BookDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperCastle.Application.Dto
+{
+    public class BookDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string CoverImageURL { get; set; }
+        public int YearOfWriting { get; set; }
+        public int? AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public int? CountryId { get; set; }
+        public string CountryName { get; set; }
+    }
+
+}

ff8d4057137b55bf052e2f22a250492bdb072cab
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-06-19 16:20:23 +0300

    added review dto

---
+++ PaperCastle.Application/Dto/ReviewDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperCastle.Application.Dto
+{
+    public class ReviewDto
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int Rating { get; set; }
+        public int BookId { get; set; }   
+        public string UserName { get; set; }
+    }
+}

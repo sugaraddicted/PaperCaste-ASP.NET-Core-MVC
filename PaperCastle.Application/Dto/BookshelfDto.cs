ba8a1309f0c6b3abad4986b169f25ab15273ad7f
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-06-19 16:19:09 +0300

    added bookshelf dto

---
+++ PaperCastle.Application/Dto/BookshelfDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperCastle.Application.Dto
+{
+    public class BookshelfDto
+    {
+        public int Id { get; set; }
+        public int ApplicationUserId { get; set; }
+        public string Name { get; set; }
+    }
+}

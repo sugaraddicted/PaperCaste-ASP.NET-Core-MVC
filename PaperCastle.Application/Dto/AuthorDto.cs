58a1078e57c12fe028d785042da25611ea38c33e
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-06-19 16:18:27 +0300

    added author dto

---
+++ PaperCastle.Application/Dto/AuthorDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperCastle.Application.Dto
+{
+    public class AuthorDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public int? CountryId { get; set; }
+        public string CountryName { get; set; }
+    }
+}

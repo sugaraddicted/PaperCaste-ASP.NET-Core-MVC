c0c4532603e09397a84e03392d49855722718c4c
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-09-18 14:14:52 +0300

    added genre view and controller

--- PaperCastle.Application/Dto/GenreDto.cs
+++ PaperCastle.Application/Dto/GenreDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,12 @@ namespace PaperCastle.Application.Dto
     public class GenreDto
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Genre name is required")]
+        [MaxLength(50, ErrorMessage = "Genre name cannot exceed 50 characters")]
         public string Name { get; set; }
+
+        [MaxLength(250, ErrorMessage = "Description cannot exceed 250 characters")]
         public string Description { get; set; }
     }
 }
---
+++ PaperCastle/Controllers/GenreController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PaperCastle.Core.Entity;
+using PaperCastle.Application.Dto;
+using PaperCastle.Infrastructure.Data.Repository;
+using PaperCastle.Infrastructure.Data.Intefaces;
+using PaperCastle.Infrastructure.Data.ViewModels;
+
+namespace PaperCastle.WebUI.Controllers
+{
+    public class GenreController : Controller
+    {
+        private readonly IGenreRepository _genreRepository;
+        private readonly IMapper _mapper;
+        public GenreController(IGenreRepository genreRepository, IMapper mapper)
+        {
+            _genreRepository = genreRepository;
+            _mapper = mapper;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(GenreDto genreDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var genre = _mapper.Map<Genre>(genreDto);
+                _genreRepository.CreateGenre(genre);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(genreDto);
+        }
+    }
+
+
+}

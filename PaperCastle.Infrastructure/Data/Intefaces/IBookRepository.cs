ef494256111b032c87a4ad9fabd5af404218f7dd
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2023-10-18 13:04:42 +0300

    updated BookRepository

--- PaperCastle.Infrastructure/Data/Intefaces/IBookRepository.cs
+++ PaperCastle.Infrastructure/Data/Intefaces/IBookRepository.cs
@@ -1,30 +1,16 @@
 ﻿using PaperCastle.Core.Entity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-
 namespace PaperCastle.Infrastructure.Data.Intefaces
 {
     public interface IBookRepository
     {
         Task<ICollection<Book>> GetBooksAsync();
         Task<Book> GetByIdAsync(int id);
-
         Task<Book> GetBookByTitleAsync(string title);
-
         Task<decimal> GetBookRatingAsync(int bookId);
-
         Task CreateAsync(Book book);
-
         bool BookExists(int id);
-
         Task UpdateAsync(int id, Book book);
-
         Task DeleteAsync(Book book);
-
         Task SaveAsync();
     }
 }
--- PaperCastle.Infrastructure/Data/Repository/BookRepository.cs
+++ PaperCastle.Infrastructure/Data/Repository/BookRepository.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using PaperCastle.Core;
 using PaperCastle.Core.Entity;
 using PaperCastle.Infrastructure.Data.Intefaces;
 
@@ -9,6 +7,7 @@ namespace PaperCastle.Infrastructure.Data.Repository
     public class BookRepository : IBookRepository
     {
         private readonly DataContext _context;
+
         public BookRepository(DataContext context)
         {
             _context = context;
@@ -16,34 +15,45 @@ namespace PaperCastle.Infrastructure.Data.Repository
 
         public async Task<ICollection<Book>> GetBooksAsync()
         {
-            return await _context.Books.OrderBy(p => p.Id)
-                                      .Include(b => b.Author)
-                                      .ToListAsync();
+            return await _context.Books
+                .OrderBy(p => p.Id)
+                .Include(b => b.Author)
+                .ToListAsync();
         }
 
         public async Task<Book> GetByIdAsync(int id)
         {
-            var book = await _context.Books.Where(b => b.Id == id)
-                                    .Include(b => b.Author)
-                                    .Include(b => b.Country)
-                                    .Include(b => b.Reviews)
-                                    .Include(b => b.BookGenres)
-                                    .Include(b => b.BookshelfBooks)
-                                    .FirstOrDefaultAsync();
+            var book = await _context.Books
+                .Where(b => b.Id == id)
+                .Include(b => b.Author)
+                .Include(b => b.Country)
+                .Include(b => b.Reviews)
+                .Include(b => b.BookGenres)
+                .Include(b => b.BookshelfBooks)
+                .FirstOrDefaultAsync();
+
             return book;
         }
 
         public async Task<Book> GetBookByTitleAsync(string title)
         {
-            var book = await _context.Books.Where(b => b.Title == title).FirstOrDefaultAsync();
+            var book = await _context.Books
+                .Where(b => b.Title == title)
+                .FirstOrDefaultAsync();
+
             return book;
         }
 
         public async Task<decimal> GetBookRatingAsync(int bookId)
         {
-            var review = await _context.Reviews.Where(r => r.Book.Id == bookId).ToListAsync();
+            var review = await _context.Reviews
+                .Where(r => r.Book.Id == bookId)
+                .ToListAsync();
 
-            if (review.Count() <= 0) return 0;
+            if (review.Count() <= 0)
+            {
+                return 0;
+            }
 
             return ((decimal)review.Sum(r => r.Rating)) / review.Count();
         }
@@ -61,14 +71,23 @@ namespace PaperCastle.Infrastructure.Data.Repository
 
         public async Task UpdateAsync(int id, Book updatedBook)
         {
-            var book = await _context.Books.Where(b => b.Id == id).FirstOrDefaultAsync();
+            var book = await _context.Books
+                .Where(b => b.Id == id)
+                .FirstOrDefaultAsync();
 
-            if (book == null) 
+            if (book == null)
+            {
                 throw new Exception("Book not found");
-            
-            if(!updatedBook.Title.IsNullOrEmpty())
-                           book.Title = updatedBook.Title;
-            await _context.BookGenres.Where(bg => bg.BookId == book.Id).ExecuteDeleteAsync();
+            }
+
+            if (!string.IsNullOrEmpty(updatedBook.Title))
+            {
+                book.Title = updatedBook.Title;
+            }
+
+            await _context.BookGenres
+                .Where(bg => bg.BookId == book.Id)
+                .ExecuteDeleteAsync();
 
             book.AuthorId = updatedBook.AuthorId;
             book.Description = updatedBook.Description;
